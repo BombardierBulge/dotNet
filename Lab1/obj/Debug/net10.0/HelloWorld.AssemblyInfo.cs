44425cb3f6d567826a16b963a37e5354c628e462
BombardierBulge <z.bartosz.z@wp.pl>
2026-03-10 20:47:32 +0100

    Organize repo files

--- Lab1/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
+++ Lab1/obj/Debug/net10.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+427ba06d8ff7b66b51a4a13ed1328c6ba0262a69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb2004b7945c4a41948020b341733c33f11c5a73")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
