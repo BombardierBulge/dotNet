959cdf14c08ca656672780da8ac0855aef4aa916
BombardierBulge <z.bartosz.z@wp.pl>
2026-03-30 08:30:18 +0200

    update for last commit

--- Lab3/WalutyApp/CurrencyService.cs
+++ Lab3/WalutyApp/CurrencyService.cs
@@ -8,11 +8,13 @@ namespace WalutyAPI
         private readonly string AppId = Environment.GetEnvironmentVariable("OPEN_EXCHANGE_RATES_APP_ID");        
         public async Task<ExchangeRateResponse> GetHistoricalRatesAsync(string date)
         {
-            // Format daty: YYYY-MM-DD
+
+            // format daty: YYYY-MM-DD
             string url = $"https://openexchangerates.org/api/historical/{date}.json?app_id={AppId}";
 
             var response = await _client.GetFromJsonAsync<ExchangeRateResponse>(url);
             return response;
         }
     }
+    
 }
